0374d69c5061d50b879d04f8d905299443a1695f
Itzik Hanan <itzikh02@gmail.com>
2024-05-20 14:27:22 +0300

    version-1.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
-import './App.css';
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  Typography
+} from "@mui/material";
+import Alert from '@mui/material/Alert';
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
+
+import "./App.css";
 
 export function App() {
   const [showSteps, setShowSteps] = useState(false);
-  const [coffeeType, setCoffeeType] = useState('Espresso');
-  const [waterAmount, setWaterAmount] = useState('');
-  const [coffeeAmount, setCoffeeAmount] = useState('');
+  const [coffeeType, setCoffeeType] = useState("");
+  const [waterAmount, setWaterAmount] = useState("");
+  const [coffeeAmount, setCoffeeAmount] = useState("");
   const [showWarning, setShowWarning] = useState(false);
 
   const fade = useSpring({
@@ -35,31 +47,71 @@ export function App() {
     <div className="coffee-machine">
       <div className="coffee-machine-body">
         <div className="coffee-machine-screen">
-          <h1 className="title">How to Make Coffee</h1>
+          <Typography variant="h2" className="title">
+            Coffee Machine
+          </Typography>
+          <br />
           <div className="options">
-            <label htmlFor="coffeeType">Select Coffee Type: </label>
-            <select id="coffeeType" value={coffeeType} onChange={handleCoffeeTypeChange}>
-              <option value="Espresso">Espresso</option>
-              <option value="Americano">Americano</option>
-              <option value="Cappuccino">Cappuccino</option>
-              <option value="Latte">Latte</option>
-            </select>
-            <label htmlFor="waterAmount">Water Amount (ml): </label>
-            <input type="number" id="waterAmount" value={waterAmount} onChange={handleWaterAmountChange} />
-            <label htmlFor="coffeeAmount">Coffee Amount (grams): </label>
-            <input type="number" id="coffeeAmount" value={coffeeAmount} onChange={handleCoffeeAmountChange} />
-            {showWarning && <p className="warning">Warning: Excessive caffeine intake can be harmful. Please drink responsibly.</p>}
+            <div className="select-wrapper">
+              <FormControl fullWidth className="select-box">
+                <InputLabel>Select coffee type</InputLabel>
+                <Select
+                  displayEmpty
+                  notched={true}
+                  id="coffeeType"
+                  value={coffeeType}
+                  onChange={handleCoffeeTypeChange}
+                >
+                  <MenuItem value="Espresso">Espresso</MenuItem>
+                  <MenuItem value="Americano">Americano</MenuItem>
+                  <MenuItem value="Cappuccino">Cappuccino</MenuItem>
+                  <MenuItem value="Latte">Latte</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <TextField
+              id="waterAmount"
+              label="Water Amount (ml)"
+              type="number"
+              value={waterAmount}
+              onChange={handleWaterAmountChange}
+              className="select-box"
+            />
+            <TextField
+              id="coffeeAmount"
+              label="Coffee Amount (grams)"
+              type="number"
+              value={coffeeAmount}
+              onChange={handleCoffeeAmountChange}
+              className="select-box"
+            />
+            {showWarning && (
+              <Alert icon={<HeartBrokenIcon fontSize="inherit" />} severity="error">
+               Warning: Excessive caffeine intake can be harmful. Please drink
+                responsibly.
+            </Alert>
+              
+            )}
           </div>
-          <button onClick={() => setShowSteps(!showSteps)} className="toggle-button">
-            {showSteps ? 'Hide Steps' : 'Show Steps'}
-          </button>
+            <br />
+          <Button
+            variant="contained"
+            onClick={() => setShowSteps(!showSteps)}
+            className="toggle-button"
+          >
+            {showSteps ? "Hide" : "Make Coffee"}
+          </Button>
         </div>
         <animated.div style={fade} className="coffee-machine-content">
           <div className="steps">
-            <h2 className="step-title">Steps:</h2>
+            <Typography variant="h2" className="step-title">
+              Steps:
+            </Typography>
             <ol>
               <li>Boil {waterAmount} ml of water in a kettle or pot.</li>
-              <li>Measure {coffeeAmount} grams of {coffeeType} coffee.</li>
+              <li>
+                Measure {coffeeAmount} grams of {coffeeType} coffee.
+              </li>
               <li>Add the coffee grounds to a coffee filter.</li>
               <li>Pour the hot water over the grounds.</li>
               <li>Let it steep for a few minutes.</li>
